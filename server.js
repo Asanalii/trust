903f7d9f3dc4f927a8f159162f8e9a17f5c39e45
Asanalii <asanaliiospannn@gmail.com>
2022-04-20 02:21:15 +0600

    check

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const ejs=require('ejs')
+
+app.set('view engine','ejs');
+
+app.use(express.static(__dirname+"/public"))
+app.use("/", require("./routes/root"));
+app.use("/login", require("./routes/login"));
+app.use("/about", require("./routes/about"));
+app.use("/signup", require("./routes/signup"));
+app.use("/article", require("./routes/article"));
+
+
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`)
+);
